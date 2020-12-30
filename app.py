dee6ab24d67fe96709c6f5e0970f94a396f25f06
preethu19 <56807948+preethu19@users.noreply.github.com>
2020-12-30 14:20:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ import json
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'mysecret'
+# app.config['SECRET_KEY'] = 'mysecret'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 login = LoginManager(app)
 login.login_view = 'login'
